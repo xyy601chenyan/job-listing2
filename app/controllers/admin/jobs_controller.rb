41c950225ee262fccaf63482c13bb7e6272ad532
xyy601chenyan <xyy545688506@gmail.com>
2017-01-20 21:47:11 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
 before_action :require_is_admin
 
 
- 
+
 
   def show
     @job=Job.find(params[:id])
@@ -47,6 +47,6 @@ before_action :require_is_admin
 
   private
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
